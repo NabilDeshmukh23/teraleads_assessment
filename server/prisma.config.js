0d854fe13b68784e62566b8156d8fff07c09656e
Nabil <nabildeshmukh2@gmail.com>
2026-02-19 22:28:50 +0530

    ..

--- server/prisma.config.js
+++ server/prisma.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "prisma/config";
 import dotenv from "dotenv";
 
-// Manually load the .env file
 dotenv.config();
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  // Ensure the datasource URL is explicitly pulled from process.env
+
   datasource: {
     url: process.env.DATABASE_URL,
   },
